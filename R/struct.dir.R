9b6a4e1808423e7a967579ddb4bd0b8dbaf31777
rosalieb <rosaliebruel@gmail.com>
2021-09-13 14:15:19 +0200

    Create a function giving the expected structure of the directory

---
+++ R/struct.dir.R
@@ -0,0 +1,33 @@
+#' @title Help showing the structure of the directory
+#' 
+#' @description Print in the console the folder structure were all the data needed to run the rhobo routine should be.
+#'
+#' @author 
+#'
+#' @export
+#' @keywords planaqua
+#' @keywords hobo
+#' @example struct.dir()
+
+struct.dir <- function() {
+  cat(paste0(
+    " Structure of the directory: \n ('...' below refers to the path2data you gave: \n   ",path2data,
+    ")\n\n .../PLANAQUA",
+    "\n        |",
+    "\n        |___ HOBO_Process",
+    "\n        |       |",
+    "\n        |       |___ old",
+    "\n        |       |     |", 
+    "\n        |       |     |___ [all the previous file]",
+    "\n        |       |",  
+    "\n        |       |___ [recent-most file for each lake]",
+    "\n        |",
+    "\n        |___ HOBO_Raw",
+    "\n                |",
+    "\n                |___ ", foldername,
+    "\n                |     |", 
+    "\n                |     |___ [one file per lake]",
+    "\n                |", 
+    "\n                |___ [one folder per field mission]\n"))
+  
+}
