939ed5cc915829c08d0d15d120daaf27c76d3b19
rosalieb <rosaliebruel@gmail.com>
2021-09-13 14:16:14 +0200

    redo the different steps and make sure the new files are appended to the correct ones

--- R/rhobo.append.R
+++ R/rhobo.append.R
@@ -2,8 +2,7 @@
 #' 
 #' @description Apply the correction factors to measured dissolved oxygen and append file for each lake to previous file. If no previous file was found, only the new file is kept.
 #' 
-#' @author Rosalie Bruel 
-#' @author Sophie Guillon
+#' @author Rosalie Bruel and Sophie Guillon
 #' 
 #' @export
 #' @param metadata_QAQC output from the rhobo.CF() function. Data frame with the correction factors for all the new HOBO files.
@@ -18,10 +17,14 @@ rhobo.append <- function(metadata_QAQC, path2data, foldername, TM_init, TM_end) 
   
   Sys.setenv(TZ = "GMT")
   
-  # 1.1- create the Append text that will be added to the output files.
-  Append = format(as.Date(foldername, format = "%Y_%m_%d"), format = "%Y%m") # To respect the format, create the append that will be add to each QAQC'd file
+  # 1- Pre computation ####
+  # 1.1- Check inputs ####
+  # Change the format of TM_init and TM_end
+  TM_init <-  parse_date_time(x = TM_init, orders = c("%Y/%m/%d %H:%M:%S","%Y-%m-%d %H:%M:%S"), tz = "GMT")
+  TM_end  <-  parse_date_time(x = TM_end, orders = c("%Y/%m/%d %H:%M:%S","%Y-%m-%d %H:%M:%S"), tz = "GMT")
+
   
-  # 1.2- Path to new files (raw): List in the files and extract name
+  # 1.2- Path to new files (raw): List in the files and extract name ####
   files_raw <- list.files(paste0(path2data, "/Hobo_Raw/", foldername), pattern = ".txt", full.names = FALSE)
   # Give an error message if no files were returned
   if(length(files_raw) == 0) stop(paste0("No raw files were found.\n1) Verify your path: \n   ", paste0(path2data, "/Hobo_Raw/", foldername),"\n   The '/Hobo_Raw/' part is automatically filled, and the folder name ",foldername,"\n   is an argument of the function. \n   If issue here, edit the first part of the function, i.e., the argument 'path2data'.",
@@ -31,29 +34,27 @@ rhobo.append <- function(metadata_QAQC, path2data, foldername, TM_init, TM_end) 
   # Get file path
   files_raw <- list.files(paste0(path2data, "/Hobo_Raw/", foldername), pattern = ".txt", full.names = TRUE)
   
-  # 1.3- Read in the previous compiled files
-  files_proccess <- list.files(paste0(path2data, "/Hobo_Process"), pattern = ".txt", full.names = FALSE)
+  # 1.3- Read in the previous compiled files ####
+  # 1.3.1- Move all the files in process to the "old" folder
+  # List files
+  files_from <- list.files(paste0(path2data, "/Hobo_Process"), pattern = ".txt", full.names = FALSE)
+  files_to <- c(paste0(path2data, "/Hobo_Process/old/", files_from))
+  # Copy all the files
+  lapply(seq_along(files_from), function (x) file.copy(from = paste0(path2data, "/Hobo_Process/",files_from)[x],
+            to = files_to[x]))
+  # Remove all the files
+  lapply(seq_along(files_from), function (x) file.remove(paste0(path2data, "/Hobo_Process/",files_from)[x]))
   
-  # Extract lake number (01, 02, 03, ..., 16)
-  lake_index <- unlist(lapply(strsplit(files_proccess,"_"), head, 1))
-  # Extract date
-  date_index <- as.Date(paste0(substr(unlist(lapply(strsplit(files_proccess,"_"), tail, 1)), 1,6), "01"), format = "%Y%m%d")
+  # 1.3.2- The "old" folder contains all the data for all the lakes. For each lakes, we'll ID the most recent dataset.
+  temp <- list.recent.files(paste0(path2data, "/Hobo_Process/old"))
+  files_process <- temp$files
+  lake_index <- temp$index
   
-  # select only one file per lake (lake_index vector), and within lake, select only the most recent file (date_index vector)
-  which2keep = NULL
-  for (i in unique(lake_index)) {
-    which2keep <- c(which2keep, which(lake_index == i)[order(date_index[which(lake_index == i)], decreasing = TRUE)[1]])
-  }
-  lake_index_process <- lake_index[which2keep]
   
-  files_proccess <- list.files(paste0(path2data, "/Hobo_Process"), pattern = ".txt", full.names = TRUE)
-  # This vector below will have path to only the most recent files.
-  files_proccess <- files_proccess[which2keep]
-  
-  # Read in data
+  #2- Create new file with appended data for each lake ####
   read_dat_process <- vector(mode = "list", length = length(files_raw))
   
-  # Correction factor data frame
+  # 2.1- Correction factor data frame ####
   CF_dt <- metadata_QAQC %>% 
     mutate(DO_CF = 
              DO_correction_factor_manual %>% 
@@ -61,32 +62,67 @@ rhobo.append <- function(metadata_QAQC, path2data, foldername, TM_init, TM_end) 
              ifelse(DO_correction_factor, DO_correction_factor_manual)) %>% 
     select(Lake, What, DO_CF)
   
-  # For each 'file_raw' (each .txt files downloaded with the HOBO software), readin and append.
+  # 2.2- Loop for each file starts here ####
+  # For each 'file_raw' (each .txt files downloaded with the HOBO software), read-in and append, when corresponding file exist.
   for (i in 1:length(files_raw)) {
     # Read in new file
     newfile <- read.hobo(files_raw[i])
-    newfile$lac <- substr(lake_index_process[i], 4, nchar(lake_index_process))
+    newfile$lac <- substr(lake_index_raw[i], 4, nchar(lake_index_raw))
     if(!is.na(as.numeric(paste(newfile$lac[1])))) newfile$lac <- as.numeric(paste(newfile$lac))
     
     # Read in previous corrected file = file to append
-    if(! lake_index_raw[i] %in% lake_index_process) stop("Couldn't find the file to append to. Lake names are different.")
-    tmppath <- files_proccess[which(lake_index_process == lake_index_raw[i])]
-    file2append <- read.delim(tmppath)
-    outpath <- substr(tmppath, 1, nchar(tmppath)-10)
+    if(lake_index_raw[i] %in% lake_index_process) {
+      tmppath <- files_proccess[which(lake_index_process == lake_index_raw[i])]
+      file2append <- read.delim(tmppath)
+     
+      outfile <- append.new.HOBO.file(newfile = newfile,file2append = file2append,
+                                      CF_init = CF_dt$DO_CF[CF_dt$Lake == lake_index_raw[i] & CF_dt$What == "Init" ],
+                                      CF_end = CF_dt$DO_CF[CF_dt$Lake == lake_index_raw[i] & CF_dt$What == "End"],
+                                      TM_init = TM_init,
+                                      TM_end = TM_end)
+    } else {
+    # If no previous file was found, just write the current newdata file alone
+      outfile <- append.new.HOBO.file(newfile = newfile,file2append = NULL,
+                                      CF_init = CF_dt$DO_CF[CF_dt$Lake == lake_index_raw[i] & CF_dt$What == "Init" ],
+                                      CF_end = CF_dt$DO_CF[CF_dt$Lake == lake_index_raw[i] & CF_dt$What == "End"],
+                                      TM_init = TM_init,
+                                      TM_end = TM_end)  
+    }
     
-    outfile <- append.new.HOBO.file(newfile = newfile,file2append = file2append,
-                                    CF_init = CF_dt$DO_CF[CF_dt$Lake == lake_index_raw[i] & CF_dt$What == "Init" ],
-                                    CF_end = CF_dt$DO_CF[CF_dt$Lake == lake_index_raw[i] & CF_dt$What == "End"],
-                                    TM_init = TM_init,
-                                    TM_end = TM_end)
+    # Create outpath name
+    start_date <- format(as.Date(outfile$datetime[1]), format = "%Y%m")
+    end_date <- format(as.Date(tail(outfile$datetime, 1)), format = "%Y%m")
+    outpath <- paste0(path2data, "/Hobo_Process/old/",lake_index_raw[i], "_data_compile_", start_date,"_", end_date,".txt")
+    
+    # Turn lake ID to character
     outfile$lac <- as.character(paste(outfile$lac))
     
-    read_dat_process[[i]]<- outfile
-    if(file.exists(paste0(outpath, Append, ".txt"))) stop(paste0("A file with this name (",outpath,Append,".txt) already exist in the folder. Delete it first if you want to overide it."))
-    write.table(outfile, file = paste0(outpath, Append, ".txt") , append = FALSE, sep = "\t", dec = ".", row.names = FALSE, col.names = TRUE)
+    # Check there is only one lake id 
+    if(length(unique(outfile$lac))>1) stop(paste0("More than one lake ID was created for ", lake_index_raw[i], ": ", paste(unique(outfile$lac), collapse = ", "),".\n Go to the root files and functions to solve the issue."))
     
-    # Return list with all the data frame
-    return(read_dat_process)
+    # Write data
+    read_dat_process[[i]]<- outfile
+    if(file.exists(outpath)) stop(paste0("A file with this name (",outpath,Append,".txt) already exist in the folder. Delete it first if you want to overide it."))
+    write.table(outfile, file = outpath , append = FALSE, sep = "\t", dec = ".", row.names = FALSE, col.names = TRUE)
+        
+    # Print progress
+    message(paste0(lake_index_raw[i],":\n     '", unlist(lapply(strsplit(outpath,split = "/"), tail, 1)), "' was written in the home folder."))
+
   }
   
+  # 3- Move most recent files to process folder ####
+  message("\n........Almost done........\n     Now moving keeping in the home folder only the recent-most file (one per lake ID).")
+  # List files
+  files_from <- list.recent.files(paste0(path2data, "/Hobo_Process/old"))$files
+  files_to <- str_replace(files_from, "/old", "")
+  # Copy all the files
+  lapply(seq_along(files_from), function (x) file.copy(from = files_from[x],
+                                                       to = files_to[x]))
+  # Remove all the files
+  lapply(seq_along(files_from), function (x) file.remove(files_from[x]))
+  
+  message("........................Done!")
+  
+  # Return list with all the data frame created in step 2.
+  return(read_dat_process)
 }
